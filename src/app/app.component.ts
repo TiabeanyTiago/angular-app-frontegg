36ae3aacb1002e7f5cdcf02be7f716e796cd26f2
Tiabeany Tiago do Espirito Santo <tiago.espiritosanto.dev@gmail.com>
2024-04-06 19:16:48 -0300

    Added hosted login and admin portal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from "rxjs";
+import { FronteggAppService, FronteggAuthService, ContextHolder } from "@frontegg/angular";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true //comment out if you're not using a standalone app
 })
-export class AppComponent {
-  title = 'angular-app-frontegg';
+export class AppComponent implements OnInit, OnDestroy {
+  isLoading = true;
+  loadingSubscription: Subscription;
+  user?: any;
+
+  constructor(private fronteggAuthService: FronteggAuthService, private fronteggAppService: FronteggAppService) {
+    this.loadingSubscription = fronteggAppService.isLoading$.subscribe((isLoading) => this.isLoading = isLoading)
+  }
+
+  ngOnInit(): void {
+    this.fronteggAuthService?.user$.subscribe((user) => {
+      this.user = user
+    })
+  }
+
+  loginWithRedirect(): void {
+    this.fronteggAuthService.loginWithRedirect();
+  }
+
+  showAdminPortal(): void {
+    this.fronteggAppService?.showAdminPortal()
+  }
+
+  logOut(): void {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+  }
+
+  ngOnDestroy(): void {
+    this.loadingSubscription.unsubscribe()
+  }
 }
